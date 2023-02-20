77751bc6f7e2e14b2862ff8742c6b8abc6e486db
changxin W <986798607@qq.com>
2023-02-21 00:08:14 +0800

    new

--- mgetool/cli/cpdir.py
+++ mgetool/cli/cpdir.py
@@ -12,6 +12,13 @@ from path import Path
 
 _dos_help = r"""根据路径文件,复制文件夹到新路径下, 查看帮助使用 -h.
 复制多个文件夹到新位置。
+
+1.
+    cpdir -m temp -force
+
+2.
+    cpdir -z
+
 """
 
 def _copy_user(i, j, force=False):
@@ -24,27 +31,34 @@ def _copy_user(i, j, force=False):
         shutil.copy(i, j)
 
 
-def copyfile(oldpath, newpath, oldpaths, force=False):
+def copyfile(oldpath, newpath, oldpaths, force=False, z=False, mv=True):
 
     oldpath = Path(oldpath).abspath()
-    newpath = Path(newpath).abspath()
-
     oldpaths2 = [oldpath.relpathto(Path(i).abspath()) for i in oldpaths]
 
-    newpaths = [Path.joinpath(newpath, i) for i in oldpaths2]
+    if mv:
+        newpath = Path(newpath).abspath()
+        newpaths = [Path.joinpath(newpath, i) for i in oldpaths2]
 
-    for i, j in zip(oldpaths, newpaths):
+        for i, j in zip(oldpaths, newpaths):
+            try:
+                _copy_user(i, j, force)
+                print(f"copy: {i}.")
+            except FileExistsError as e:
+                print(f"Error to copy: \n{i} \nto \n{j}.")
+                print('You could try use "-force" to cover the exist file.')
+                raise e
+
+            except BaseException as e:
+                print(e)
+                print(f"Error to copy: \n{i} \nto \n{j}.")
+
+    if z:
         try:
-            _copy_user(i, j, force)
-            print(f"copy: {i}.")
-        except FileExistsError as e:
-            print(f"Error to copy: \n{i} \nto \n{j}.")
-            print('You could try use "-force" to cover the exist file.')
-            raise e
-
+            ff = " ".join(oldpaths2)
+            os.system(f"tar -zcvf temp.tar.gz {ff}")
         except BaseException as e:
             print(e)
-            print(f"Error to copy: \n{i} \nto \n{j}.")
 
 
 class CLICommand:
@@ -57,24 +71,31 @@ class CLICommand:
         parser.add_argument('-op', '--old_path', help='old path.', type=str, default=".")
         parser.add_argument('-np', '--new_path', help='new path.', type=str, default=".")
         parser.add_argument('-force', '--force', help='disk exist cover.', action="store_true")
+        parser.add_argument('-z', '--z', help='tar.gz.', action="store_true")
+        parser.add_argument('-m', '--m', help='move.', action="store_true")
+
 
     @staticmethod
     def run(args, parser):
+
+        assert any((args.z, args.m)), "Please choose one in {-m: copy disk/files, -z: zip disk/files}"
+
         if args.new_path_arg is not None:
             new_path = args.new_path_arg
         else:
             new_path = args.new_path
 
         if args.old_path == new_path:
-            pass
-        else:
-            with open(args.path_file) as f:
-                w = f.readlines()
-            if len(w) == 0:
-                return
-            w = [i.rstrip() for i in w]
+            if args.m:
+                raise NotImplementedError("Please offer one target path to copy.")
 
-            copyfile(args.old_path, new_path, w, args.force)
+        with open(args.path_file) as f:
+            w = f.readlines()
+        if len(w) == 0:
+            return
+        w = [i.rstrip() for i in w]
+
+        copyfile(args.old_path, new_path, w, args.force, z=args.z, mv=args.m)
 
     @staticmethod
     def parse_args(parser):
